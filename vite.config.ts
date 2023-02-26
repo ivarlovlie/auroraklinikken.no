9497cb81b6f7d95254e4a86e9862de5631e5041b
iv-ar <i@oiee.no>
2023-02-26 17:07:06 +0100

    fix: Remove unused vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
-import svelteSVG from "vite-plugin-svelte-svg";
 
 export default defineConfig({
-	plugins: [sveltekit(), svelteSVG()],
+	plugins: [sveltekit()],
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"],
 	},
